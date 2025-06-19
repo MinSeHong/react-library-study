fdc7bd8390e96fa45540141b885ea86043ff1b60
MinSeHong <rebellia@naver.com>
2025-06-19 11:12:58 +0900

    Update page.tsx

--- app/three/page.tsx
+++ app/three/page.tsx
@@ -1,12 +1,14 @@
 // app/page.tsx 또는 다른 페이지에서
 import dynamic from 'next/dynamic';
 import ThreeScene from './threescene';
+import Header from '../_components/header';
 
 export default function HomePage() {
   return (
-    <main>
-      <h1>Hello Three.js in Next.js!</h1>
+    <>
+      <Header/>
+      <h1 style={{textAlign:"center", paddingTop:"40px"}}>Hello Three.js in Next.js!</h1>
       <ThreeScene />
-    </main>
+    </>
   );
 }
